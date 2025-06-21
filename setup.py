cb81178d596a64018ab968779adc42b5a93ab9ee
RodrigodcRocha <vadan.2r@gmail.com>
2025-06-20 21:23:51 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ with open("README.md", "r", encoding="utf-8") as f:
 setup(
     name='estatistica_simples',
     version='0.1.0',
-    author='Seu Nome',
-    author_email='seu.email@example.com',
+    author='Rodrigo Rocha',
+    author_email='vadan.2r@gmail.com',
     description='Um pacote Python simples para cálculos estatísticos básicos.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/seu_usuario/estatistica_simples',  # Substitua pelo seu repositório
+    url='https://github.com/vadan2r/estatistica_simples',
     packages=find_packages(),  # Encontra automaticamente os pacotes
     classifiers=[
         "Programming Language :: Python :: 3",
